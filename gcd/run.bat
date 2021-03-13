e870a24d88078c12802a33ae46fecfc8d8f75d84
VgTajdd <aduranddiaz@gmail.com>
2021-03-13 08:23:17 -0500

    Update run.bat (use vs 2019)
    Also update .gitignore for considering obj files

--- gcd/run.bat
+++ gcd/run.bat
@@ -1,4 +1,4 @@
-set VS2017AUXBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\
+set VS2019AUXBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\
 
-@call "%VS2017AUXBUILD%vcvarsall.bat" x64 %*
+@call "%VS2019AUXBUILD%vcvarsall.bat" x64 %*
 @call nmake
